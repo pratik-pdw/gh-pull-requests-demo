c1523b54b656a98fc73b16cf07ca0c484d0d82e3
Pratik Wadekar <32261562+pratik-pdw@users.noreply.github.com>
2025-05-27 15:32:28 +0000

    cleaned app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
-import { useState } from 'react'
 import { FcShipped } from "react-icons/fc";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <FcShipped size={84}/>
       </div>
       <h1>Vite + React + Github Pages</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          Click to increment: {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
